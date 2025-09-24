6a464007488664135050bf1f72eda89f4dfef05d
teusxly <matheus006sampaio@gmail.com>
2025-09-24 13:02:01 -0300

    first commit

---
+++ src/main/java/dev/teusxly/projetoCadastroDePessoas/ProjetoCadastroDePessoasApplication.java
@@ -0,0 +1,13 @@
+package dev.teusxly.projetoCadastroDePessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCadastroDePessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCadastroDePessoasApplication.class, args);
+	}
+
+}
